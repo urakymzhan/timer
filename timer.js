d54e47a0c0837f16419dfeaee87b89fa39bea854
Ulan Rakymzhanov <ulan@carta.healthcare>
2019-09-12 13:44:24 -0700

    first commit

---
+++ timer.js
@@ -0,0 +1,73 @@
+
+const secondSpan = document.getElementById('seconds');
+const minuteSpan = document.getElementById('minutes');
+const hourSpan = document.getElementById('hours');
+const restart = document.getElementById('restart');
+const pause = document.getElementById('pause');
+const start = document.getElementById('start');
+
+
+function mintimer(min) {
+    var hr = 0;
+    if (min < 9) {
+        minuteSpan.innerHTML = "0:"+ min
+    }
+    else if (min == 60) {
+        hr++;
+        min = 0;
+        minuteSpan.innerHTML = "0:"+ min;
+        hrtimer(hr);
+    }
+    else {
+        minuteSpan.innerHTML = min;
+    }
+}
+
+function hrtimer(hr) {
+    if (hr < 9) {
+        hourSpan.innerHTML = "0:"+ hr
+    }
+    else {
+        hourSpan.innerHTML = hr;
+    }
+}
+
+function timer(){
+    var sec = 0;
+    var min = 0;
+    var timerr = setInterval(function() {      
+       
+        if (sec < 9) {
+            secondSpan.innerHTML='0:'+ sec;
+        } 
+        else if (sec == 60){
+            sec = 0;
+            min++;
+            secondSpan.innerHTML = "0:" + sec;
+            mintimer(min);
+        }
+        else {
+            secondSpan.innerHTML=sec;
+        }
+        sec++;
+
+    }, 1000);
+} 
+
+
+
+// start
+start.addEventListener("click", timer);
+
+
+// pause
+// var obj = new timer()
+// pause.addEventListener('click', function() {
+//     clearInterval(obj.timerr());
+// }) 
+
+
+// restart 
+// restart.addEventListener("click", function(){ });
+
+
